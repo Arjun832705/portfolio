7d9cdd1ae6b62141da01825618bc52a658e11634
Arjun832705 <arjunsolanki88k@gmail.com>
2026-04-24 20:50:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Education from './components/Education';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import { motion, useScroll, useSpring } from 'motion/react';
